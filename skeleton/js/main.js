93726e02bbcdd9dcff2ebcbae05258944b1e13a8
nacl <uhohnaomi@gmail.com>
2024-06-08 11:05:28 -0700

    Gestures

--- skeleton/js/main.js
+++ skeleton/js/main.js
@@ -11,4 +11,23 @@ document.addEventListener("DOMContentLoaded", function(){
             }
         });
     }, 1000);
+
+    const primary = document.getElementById("page-tray");
+    if (primary !== null) {
+        var mc = new Hammer(primary, {
+            domEvents: true,
+        });
+
+        mc.on("swipeleft swiperight", function(ev) {
+            const prev_link = document.getElementById('prev-link')
+            const next_link = document.getElementById('next-link')
+            if (ev.type === "swiperight" && prev_link !== null) {
+                prev_link.click();
+            } else if (ev.type === "swipeleft" && next_link !== null) {
+                next_link.click();
+            }
+
+        });
+        mc.get('pinch').set({ enable: false });
+    }
 });
